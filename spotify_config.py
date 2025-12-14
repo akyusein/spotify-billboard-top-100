b277955dedaa7165035049202814043666b3457b
Aksel Yusein <akselyusein4@gmail.com>
2025-12-14 15:55:54 +0000

    SPOB-09

--- spotify_config.py
+++ spotify_config.py
@@ -14,7 +14,7 @@ class SpotifyConfig:
         track_uris = []
         for index, value in collected_info.items():
             search_params = {
-                "q": f"{index} {value}",
+                "q": f"track:{value}, artist:{index}",
                 "type": "track",
                 "market": "ES",
                 "limit": 1,
@@ -23,9 +23,7 @@ class SpotifyConfig:
             next_response = requests.get(url=self.search_endpoint, params=search_params, headers=self.auth)
             if data := next_response.json()["tracks"]["items"]:
                 for track in data:
-                    for artist in track["artists"]:
-                        if artist["name"].lower() in index and track["uri"] not in track_uris:
-                            track_uris.append(track["uri"])
+                    track_uris.append(track["uri"])
         return track_uris
 
     def create_playlist(self, uris):
